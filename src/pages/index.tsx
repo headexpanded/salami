383282dc7dfa66f4539732536539e2a1047eb267
MarkH <markh@bellemoncon>
2023-04-05 23:17:43 +0200

    remove template Next divs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,42 +17,42 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <AddRecipe />
-        </div>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+          <div className={styles.grid}>
+            <Image
+              className={styles.logo}
+              src="/salami-02.png"
+              alt="Next.js Logo"
+              width={100}
+              height={100}
+              priority
+            />
+            <AddRecipe />
           </div>
-        </div>
+          {/*<div className={styles.description}>
+             <p>
+              Get started by editing&nbsp;
+              <code className={styles.code}>src/pages/index.tsx</code>
+            </p> */}
+            {/* <div>
+              <a
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                By{" "}
+                <Image
+                  src="/vercel.svg"
+                  alt="Vercel Logo"
+                  className={styles.vercelLogo}
+                  width={100}
+                  height={24}
+                  priority
+                />
+              </a>
+            </div> */}
+          </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
+          {/* <div className={styles.thirteen}>
             <Image
               src="/thirteen.svg"
               alt="13"
@@ -61,10 +61,10 @@ export default function Home() {
               priority
             />
           </div>
-        </div>
+        </div>*/}
 
         <div className={styles.grid}>
-          <a
+          {/* <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
@@ -76,9 +76,9 @@ export default function Home() {
             <p className={inter.className}>
               Find in-depth information about Next.js features and&nbsp;API.
             </p>
-          </a>
+          </a> */}
 
-          <a
+          {/* <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
@@ -90,9 +90,9 @@ export default function Home() {
             <p className={inter.className}>
               Learn about Next.js in an interactive course with&nbsp;quizzes!
             </p>
-          </a>
+          </a> */}
 
-          <a
+          {/* <a
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
@@ -104,9 +104,9 @@ export default function Home() {
             <p className={inter.className}>
               Discover and deploy boilerplate example Next.js&nbsp;projects.
             </p>
-          </a>
+          </a> */}
 
-          <a
+          {/* <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
@@ -119,9 +119,9 @@ export default function Home() {
               Instantly deploy your Next.js site to a shareable URL
               with&nbsp;Vercel.
             </p>
-          </a>
+          </a> */}
         </div>
       </main>
     </>
-  )
+  );
 }
