675114bbc6d78d689229615b1336ef27a6ac6328
MarkH <markh@bellemoncon>
2023-04-08 00:27:59 +0200

    ternary display for isCuring

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,39 @@
-import prisma from "../../lib/prisma";
+"use client";
+import { useState } from "react";
 import "../styles/globals.css";
+import Link from "next/link";
 import LoadingPage from "@/loading";
 
 export default function Page() {
+  const [isCuring, setIsCuring] = useState(false);
   return (
     <div class="card">
-      <div className="sub-container animated fadeInDown">
-        <h3>Now Curing:</h3>
-        <div className="sub-container-detail">
-          <p>Recipe: Oak-Smoked Italian</p>
-          <p>Start Date: 2023-04-07 14:35:54</p>
-          <p>Req. Temp: 23</p>
-          <p>Current Temp: 21.5</p>
-          <p>Req. Humidity: 14</p>
-          <p>Current Humidity: 16</p>
-          <p>Next Change: 2023-04-08 19:00:00</p>
+      {isCuring ? (
+        <div className="sub-container animated fadeInDown">
+          <h3>Now Curing:</h3>
+          <div className="sub-container-detail">
+            <p>Recipe: Oak-Smoked Italian</p>
+            <p>Start Date: 2023-04-07 14:35:54</p>
+            <p>Req. Temp: 23</p>
+            <p>Current Temp: 21.5</p>
+            <p>Req. Humidity: 14</p>
+            <p>Current Humidity: 16</p>
+            <p>Next Change: 2023-04-08 19:00:00</p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="sub-container animated fadeInDown">
+          <p>No salami is being cured at the moment.</p>
+          <p>Do some curing!</p>
+          <div className="sub-container-detail">
+            <Link href="/profile/recipe">
+              <button className="btn">
+                Set Recipe
+              </button>
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
