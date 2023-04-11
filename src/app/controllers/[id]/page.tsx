c3f1d2ce17adc74c996bf6e111476a8412e26c7e
MarkH <markh@bellemoncon>
2023-04-12 00:59:47 +0200

    controller id folder for dynamic routing

---
+++ src/app/controllers/[id]/page.tsx
@@ -0,0 +1,25 @@
+
+
+import Link from "next/link";
+import CurrentData from "../../../../Components/Shared/CurrentData";
+
+type ActiveControllerPageProps = {
+  params: {
+    controllerId: number;
+  };
+};
+
+const ActiveControllerPage = ({ params: { controllerId } }: ActiveControllerPageProps) => {
+  return (
+    <div className="card">
+      <h3>Current Data</h3>
+      <CurrentData controllerId={controllerId} />
+      <div className="links">
+        <Link href="/controllers" className="btn">
+          Close
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default ActiveControllerPage;
