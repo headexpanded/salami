aff0aab9125b91e4a61f6522d51dfa3104d75f1f
MarkH <markh@bellemoncon>
2023-04-13 21:54:15 +0200

    Controller connect/disconnect + POST route

--- Components/Shared/Controller.tsx
+++ Components/Shared/Controller.tsx
@@ -1,12 +1,13 @@
 // Calls all controllers from the API router
 // Puts them in individual list items.
+"use client";
 
-
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import CurrentData from "./CurrentData";
 
 interface Controller {
-  id: number | null;
+  id: number;
   name: string;
   description: string;
   publicIpAddress: string;
@@ -15,13 +16,83 @@ interface Controller {
 }
 
 async function fetchControllers(): Promise<Controller[]> {
-  const response = await fetch("http://localhost:3000/api/controllers");
+  const response = await fetch("/api/controllers");
   const controllers: Controller[] = await response.json();
   return controllers;
 }
 
-const Controller = async () => {
-  const controllers = await fetchControllers();
+const Controller = () => {
+  const [controllers, setControllers] = useState<Controller[]>([]);
+
+  const handleConnectClick = async (id: number) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/controllers`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id, isActive: true }),
+      });
+      if (response.ok) {
+        console.log(response);
+        const updatedController = await response.json();
+        setControllers((prevControllers) => {
+          return prevControllers.map((controller) => {
+            if (controller.id === updatedController.id) {
+              return { ...controller, isActive: true };
+            } else {
+              return controller;
+            }
+          });
+        });
+      } else {
+        console.log(response);
+        throw new Error("Error updating controller: ${response.statusText}");
+      }
+    } catch (error: unknown) {
+      console.error(error);
+    }
+  };
+
+  const handleDisConnectClick = async (id: number) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/controllers`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id, isActive: false }),
+      });
+      if (response.ok) {
+        console.log(response);
+        const updatedController = await response.json();
+        setControllers((prevControllers) => {
+          return prevControllers.map((controller) => {
+            if (controller.id === updatedController.id) {
+              return { ...controller, isActive: false };
+            } else {
+              return controller;
+            }
+          });
+        });
+      } else {
+        console.log(response);
+        throw new Error("Error updating controller: ${response.statusText}");
+      }
+    } catch (error: unknown) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchControllerData = async () => {
+      const controllers = await fetchControllers();
+      setControllers(controllers);
+    };
+
+    fetchControllerData();
+  }, []);
+
   return (
     <ul className="recipe-list">
       {controllers.map((controller) => (
@@ -31,19 +102,32 @@ const Controller = async () => {
             <p>{controller.description}</p>
             <p>{controller.publicIpAddress}</p>
             {controller.isActive ? (
-              <p>
-                <strong>Active</strong>
-              </p>
+              <div className="sub-container-detail">
+                <p>
+                  <strong>Active</strong>
+                </p>
+                <button
+                  className="btn btn-action btn-warning"
+                  onClick={() => handleDisConnectClick(controller.id)}
+                >
+                  Disconnect
+                </button>
+              </div>
             ) : (
               <div className="sub-container-detail">
                 <p>Not active</p>{" "}
-                <button className="btn btn-action">Activate</button>
+                <button
+                  className="btn btn-action"
+                  onClick={() => handleConnectClick(controller.id)}
+                >
+                  Connect
+                </button>
               </div>
             )}
           </Link>
           {controller.isActive ? (
             <div>
-              <CurrentData controllerId={1} />
+              <CurrentData controllerId={controller.id} />
             </div>
           ) : (
             <div></div>
--- src/app/api/controllers/route.ts
+++ src/app/api/controllers/route.ts
@@ -2,11 +2,30 @@
 
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
 export async function GET(request: Request) {
   try {
     const controllers = await prisma.controller.findMany();
     return new Response(JSON.stringify(controllers));
   } catch (error: unknown) {
     throw new Error(`Error fetching recipe: + ${error}`);
+  } finally {
+    await prisma.$disconnect();
   }
 }
+
+export async function POST(request: Request) {
+  const { id, isActive } = await request.json();
+  try {
+    const updatedController = await prisma.controller.update({
+      where: { id },
+      data: { isActive },
+    });
+    return new Response(JSON.stringify(updatedController));
+  } catch (error: unknown) {
+    throw new Error(`Error updating controller: ${error}`);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
