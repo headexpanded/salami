2f876c88844c826220ed0d80ef97a8fa02fe577e
MarkH <markh@bellemoncon>
2023-04-14 00:38:07 +0200

    fixed menu layout

--- Components/Shared/Header.tsx
+++ Components/Shared/Header.tsx
@@ -1,23 +1,24 @@
-
-
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    
+    <>
       <header className="header">
-            <div className="logo">
-              <Link href="/">Salami</Link>
-            </div>
-        <div className="container">
-          <div className="links">
-          <Link href="/controllers">Controllers</Link>
-            <Link href="/profile/recipes">Recipes</Link>
-          </div>
-          </div>
         
+          <div className="logo">
+            <Link href="/">Salami</Link>
+        </div>
+          
+          <div className="container">
+            <div className="links">
+              <Link href="/controllers">Controllers</Link>
+              <Link href="/profile/recipes">Recipes</Link>
+            </div>
+          </div>
       </header>
-    
+    </>
   );
 };
 
