ada8dc0683d9f7f2b79ba92df01f8b8f0f0396e3
MarkH <markh@bellemoncon>
2023-04-18 17:14:41 +0200

    head.tsx: content for <head/>

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Salami</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Salami: a home-made app for curing home-made salami"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
