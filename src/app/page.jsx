c09018b4c1017cb08dc9a25d1fd41cb65bfeb5ce
moncon <markh@bellemoncon>
2023-05-11 21:29:02 +0200

    NextJs 13.4

---
+++ src/app/page.jsx
@@ -0,0 +1,65 @@
+'use client';
+import { useSession } from 'next-auth/react';
+import '@/styles/globals.css';
+import Link from 'next/link';
+//import Controller from './controllers/Controller';
+
+// Main app page
+
+const showCuring = () => {
+  return (
+    <div className="sub-container animated fadeInDown">
+      <h3>Now Curing:</h3>
+      {/* <Controller controllerId={1} /> */}
+    </div>
+  );
+};
+
+const notCuring = () => {
+  const chefId = 1;
+  return (
+    <div>
+      {chefId ? (
+        <div className="sub-container animated fadeInDown">
+          <div className="sub-container-detail">
+            <p>No salami is being cured at the moment.</p>
+            <p>Do some curing!</p>
+            <Link href="/controllers">
+              <button className="btn">Select Controller</button>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div className="sub-container animated fadeInDown">
+          <div class="sub-container-detail">
+            <p>Get curing!</p>
+            <Link href="/controllers/addController">
+              <button className="btn">Add New Controller</button>
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const HomePage = () => {
+  const { data: session } = useSession({
+    required: true,
+  });
+
+  return (
+    <div>
+      {/* <div className="card">{notCuring()}</div> */}
+      <div className="card">
+        {session ? (
+          <h2>Welcome to Salami, {session?.user?.name}</h2>
+        ) : (
+          <h2>Welcome to Salami</h2>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
